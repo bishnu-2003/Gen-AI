c9755dbce5e53711d9fff4bfe4503dfe28f4fbc2
bishnu-2003 <bishnugope4844@gmail.com>
2025-09-05 16:15:34 +0530

    mutetion

---
+++ Chapter_1/mutetion.py
@@ -0,0 +1,9 @@
+spice_mix = set()
+print(f"initial spice mix id: {id(spice_mix)}")
+print(f"initial spice mix id: {id(spice_mix)}")
+spice_mix.add("cumin")
+spice_mix.add("turmeric")
+spice_mix.add("coriander")
+print(f"After spice mix id: {id(spice_mix)}")
+print(f"Initial spice mix id: {spice_mix}")
+
